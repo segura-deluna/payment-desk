ddad25d69b4e8e80a9edfecc0e877df94cdeb631
Segura <segura-deluna@mail.ru>
2022-09-18 22:17:25 +0300

    stylised first component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import CostItem from './components/CostItem';
 
 function App() {
   return (
     <div>
       <h1>Начнём писать доску расходов!</h1>
+      <CostItem></CostItem>
     </div>
   );
 }
