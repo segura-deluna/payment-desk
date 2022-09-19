9de425ef65c99a27bbe3593909b07c532b7eda4e
Segura <segura-deluna@mail.ru>
2022-09-19 09:51:22 +0300

    start components with props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import CostItem from './components/CostItem';
 
 function App() {
+
+  const costs = [
+    {
+      date:new Date(2021, 2, 12),
+      description:'Холодильник',
+      amount: 999.99,
+    },{
+      date:new Date(2021, 5, 1),
+      description:'Yoga Book',
+      amount: 1050.99,
+    },{
+      date:new Date(2021, 10, 19),
+      description:'Джинсы',
+      amount: 50.25,
+    },
+  ]
+
   return (
     <div>
       <h1>Начнём писать доску расходов!</h1>
-      <CostItem></CostItem>
+      <CostItem
+          date={costs[0].date}
+          description={costs[0].description}
+          amount={costs[0].amount}
+      ></CostItem>
+      <CostItem
+          date={costs[1].date}
+          description={costs[1].description}
+          amount={costs[1].amount}
+      ></CostItem>
+      <CostItem
+          date={costs[2].date}
+          description={costs[2].description}
+          amount={costs[2].amount}
+      ></CostItem>
     </div>
   );
 }
--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,12 +1,13 @@
 import './CostItem.css'
 
-function CostItem(){
+function CostItem(props){
+
   return (
       <div className='cost-item'>
-        <div>Март 2021 12</div>
+        <div>{props.date.toISOString()}</div>
         <div className='cost-item__description'>
-          <h2>Холодильник</h2>
-          <div  className='cost-item__price'>$999.99</div>
+          <h2>{props.description}</h2>
+          <div  className='cost-item__price'>${props.amount}</div>
         </div>
       </div>
   )
