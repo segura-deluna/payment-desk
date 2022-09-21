8dcb703b8af98ab6eea0517b5b2fe66d40843dd5
Segura <segura-deluna@mail.ru>
2022-09-22 02:07:03 +0300

    props&components

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,10 +1,11 @@
-import './CostItem.css'
+import './CostItem.css';
+import CostDate from './CostDate';
 
 function CostItem(props){
 
-  return (
+      return (
       <div className='cost-item'>
-        <div>{props.date.toISOString()}</div>
+        <CostDate date={props.date}/>
         <div className='cost-item__description'>
           <h2>{props.description}</h2>
           <div  className='cost-item__price'>${props.amount}</div>
